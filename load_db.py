2ccd3bac3c384013a314c73719ac18376279c3aa
Chris-Adigwe <chris.adigwe@yahoo.com>
2023-03-18 08:16:01 +0100

    imported create_engine, env_values and get_database_conn function

--- load_db.py
+++ load_db.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
 from util import get_database_conn
+from sqlalchemy import create_engine
+from dotenv import dotenv_values
+
 
 def load_to_db():
     engine = get_database_conn()
+    print(engine)
     df= pd.read_csv('Goalbet_project/data/transformed_file.csv') # Read csv file
     df.to_sql('GoalBet', con= engine, if_exists='replace', index= False)
     print('Data successfully written to PostgreSQL database')
