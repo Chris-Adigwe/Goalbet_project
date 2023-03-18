ac5bdc7e832a1807f52b5650ff30438c3276dcb1
Chris-Adigwe <chris.adigwe@yahoo.com>
2023-03-18 08:18:44 +0100

    fixed a directory error on the files relating to getting authentication values

--- util.py
+++ util.py
@@ -5,12 +5,11 @@ dotenv_values()
 
 # Database credentials
 def get_database_conn():
-    config = dict(dotenv_values('.env'))
+    config = dict(dotenv_values('Goalbet_project/.env'))
     db_user_name = config.get('DB_USER_NAME')
     db_password = config.get('DB_PASSWORD')
     db_name = config.get('DB_NAME')
     port = config.get('PORT')
     host = config.get('HOST')
-    return create_engine(f'postgresql+psycopg2://{db_user_name}:{db_password}@{host}:{port}/{db_name}')
-
+    return (create_engine(f'postgresql+psycopg2://{db_user_name}:{db_password}@{host}:{port}/{db_name}'))
 
