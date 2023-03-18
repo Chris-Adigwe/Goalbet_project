a4e62aa6c32f6530d03c7b9a9376f3995e69d5e1
Chris-Adigwe <chris.adigwe@yahoo.com>
2023-03-18 02:19:10 +0100

    created a transform file to clean null values in the time column

---
+++ transform.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+# Data load transform and load layer
+def transform():
+    col_names = ['Div','Date','Time','HomeTeam','AwayTeam','FTHG','FTAG']
+    df= pd.read_csv('Goalbet_project/data/file.csv',usecols=col_names) # Read csv file
+    if df['Time'].isna().any():
+        # Fill the missing values with the mean value
+        mean_time = df['Time'].mean()
+        df['Time'] = df['Time'].fillna(mean_time)
+
+    df.to_csv('Goalbet_project/data/transformed_file.csv', index= False)
