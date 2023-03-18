c7e704d0379af2d90a9f46701f49cc2ff8f42fe9
Chris-Adigwe <chris.adigwe@yahoo.com>
2023-03-18 08:17:21 +0100

    added a function to change the data column to datetype but its didnt work

--- transform.py
+++ transform.py
@@ -9,5 +9,6 @@ def transform():
         # Fill the missing values with the mean value
         mean_time = df['Time'].mean()
         df['Time'] = df['Time'].fillna(mean_time)
+        df['Date'] = pd.to_datetime(df['Date'])
 
     df.to_csv('Goalbet_project/data/transformed_file.csv', index= False)
